f436d684ada5cfd436232830bdfbc5a902f98a3c
nakamura0736 <tel07360102@gmail.com>
2020-11-29 17:18:18 +0900

    add privacy page etc

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,38 @@
 <template>
-  <div id="app">
-    <router-link to="/">ホーム</router-link>
-    <router-link to="/access">Access</router-link>
-    <router-link to="/Contact">contact</router-link>
+  <div id='app'>
+    <b-container fluid='xl'>
+      <b-row id='top' class='mt-3 mb-2'>
+        <b-col id='top-logo'>
+          <img src="./assets/img/top_logo.png" alt="top_logo">
+        </b-col>
+        <b-col id='top-access'>
+          <p>TEL. <a href="tel:117">080-0000-0000</a></p><br>
+          <p>亀岡の空き家相談所</p><br>
+          <p>〒000-0000</p><br>
+          <p>兵庫県丹波市柏原町南多田469-1</p>
+        </b-col>
+      </b-row >
+      <b-navbar id='navbar' toggleable='lg' type='dark' variant='primary'>
+        <b-navbar-brand class='mb-0' to='/'>空き家相談所</b-navbar-brand>
 
-    <router-view/>
+        <b-navbar-toggle target='nav-collapse'></b-navbar-toggle>
+
+        <b-collapse id='nav-collapse' is-nav>
+          <b-navbar-nav class='font-small'>
+            <b-nav-item to='/concept'>空き家相談所とは？</b-nav-item>
+            <b-nav-item to='/questions'>よくある質問</b-nav-item>
+            <b-nav-item to='/voices'>お客様の声</b-nav-item>
+            <b-nav-item to='/contact'>ご相談はこちら</b-nav-item>
+            <b-nav-item to='/access'>アクセス</b-nav-item>
+            <b-nav-item to='/privacy'>個人情報保護ポリシー</b-nav-item>
+          </b-navbar-nav>
+        </b-collapse>
+      </b-navbar>
+    
+
+      <router-view/>
+
+    </b-container>
   </div>
 </template>
 
@@ -21,6 +49,28 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  /* margin-top: 60px; */
+}
+#top-access {
+  line-height: 0.8;
+  padding-top: 10px;
+}
+#top-access p{
+  text-align: right;
+  margin-top: 0;
+  margin-bottom: 0;
+  padding-top: 0;
+  padding-bottom: 0;
+}
+#top-logo{
+  padding-left: 0;
+  /* margin-left: 0; */
+
+}
+#top {
+  /* background-color: tan; */
+}
+#navbar {
+  /* background-color: red; */
 }
 </style>
--- src/components/privacy.vue
+++ src/components/privacy.vue
@@ -1,11 +1,85 @@
 <template>
-   <div>
-      <h1>個人情報保護について</h1>
+   <div class='pg-privacy'>
+      <div id='pg-header' class="pb-0 pt-4 mb-4 border-bottom">
+         <h1>個人情報保護について</h1><br>
+      </div>
+      <div id='pg-body'>
+         <div class='policy-items pb-4'>
+            <p>
+               亀岡の空き家相談所（以下当事務所）では、個人情報に関する法令およびその他の規範を遵守し<br>
+               お客様の大切な個人情報の保護に万全を尽くします。
+            </p>
+         </div>
+
+         <div class='policy-items pb-4'>
+            <h3>個人情報の収集について</h3>
+            <p>
+               当事務所では、次のような場合に必要な範囲で個人情報を収集することがあります。
+            </p>
+            <ul>
+               <li>当事務所へのお問い合わせ時</li>
+               <li>当事務所へのサービスお申し込み時</li>
+            </ul>
+         </div>
+         <div class='policy-items pb-4'>
+            <h3>個人情報の利用目的について</h3>
+            <p>
+               当事務所は、お客様から収集した個人情報を次の目的で利用いたします。
+            </p>
+            <ul>
+               <li>お客様への連絡のため</li>
+               <li>お客様からのお問い合せに対する回答のため</li>
+               <li>お客様へのサービス提供のため</li>
+            </ul>  
+         </div>
+
+         <div class='policy-items pb-4'>
+            <h3>個人情報の第三者への提供について</h3>
+            <p>
+               当事務所では、お客様より取得した個人情報を第三者に開示または提供することはありません。
+               <br>ただし、次の場合は除きます。
+            </p>
+            <ul>
+               <li>ご本人の同意がある場合</li>
+               <li>警察からの要請など、官公署からの要請の場合</li>
+               <li>法律の適用を受ける場合</li>
+            </ul>  
+         </div>
+
+         <div class='policy-items  pb-4'>
+            <h3>個人情報の開示、訂正等について</h3>
+            <p>当事務所は、お客様ご本人からの自己情報の開示、訂正、削除等のお求めがあった場合は、確実に応じます。</p>
+         </div>
+
+         <div id='contact' class='policy-items  pb-4'>
+            <h3>個人情報保護に関するお問い合わせ先</h3>
+            <p>TEL. <a href="tel:117">080-0000-0000</a></p>
+            <p>E-Mail. <a href='mailto:xxxxxxx&#64;gmail.com'>xxxxxxx&#64;gmail.com</a></p>
+            <p>亀岡の空き家相談所　代表　山田太郎</p>
+         </div>
+      </div>
    </div>
+
 </template>
 
 <script>
 </script>
 
 <style>
+#pg-header{
+text-align: center
+}
+#pg-header h1{
+   /* line-height: 36px; */
+}
+#pg-body{
+   text-align: left;
+}
+#contact {
+  /* line-height: 0.8; */
+  /* padding-top: 10px; */
+}
+#contact p{
+  margin-bottom: 0;
+}
 </style>
