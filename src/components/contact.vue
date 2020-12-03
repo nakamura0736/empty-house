9bd089e8d7db360d6ee695b6b753a08525aa3ad0
nakamura0736 <tel07360102@gmail.com>
2020-12-03 22:25:13 +0900

    add form link, and adjust page

--- src/components/contact.vue
+++ src/components/contact.vue
@@ -4,35 +4,34 @@
          <h1>ご相談はこちら</h1>
       </div>
       <div id='pg-body'>
-         <div class='q-category'>
+         <div class='q-category pb-4'>
             <h3 class='text-primary mb-4'>相談フォーム</h3>
             <div class='items pb-4'>
                <!-- <h5 class='border-bottom'>相談フォームへご記入ください</h5> -->
                <h5>相談フォームへご記入ください</h5>
-               
+               <a href='https://forms.gle/2EsXZ91s8SivLL4p6' class='btn btn-primary' type='dark' target='_blank' rel='noopener noreferrer'>ご相談フォーム</a>
             </div>
-           
          </div>
-         <div class='q-category'>
+         <div class='q-category pb-4'>
             <h3 class='text-primary mb-4'>電話・FAX・メール</h3>
             <div class='items pb-4'>
                <h5 class='border-bottom'>電話番号</h5>
                <div>
                   <p>
-                     000-0000-0000
+                     <a href="tel:117">080-0000-0000</a>
                   </p>
                </div>
             </div>
             <div class='items pb-4'>
                <h5 class='border-bottom'>FAX</h5>
                <p>
-                  000-0000-0000
+                  <a href="tel:117">080-0000-0000</a>
                </p>
             </div>
             <div class='items pb-4'>
                <h5 class='border-bottom'>メールアドレス</h5>
                <p>
-                  xxxx@gmail.com
+                  <a href='mailto:xxxxxxx@gmail.com'>xxxxxxx&#64;gmail.com</a>
                </p>
             </div>
          </div>       
@@ -45,5 +44,4 @@
 </script>
 
 <style>
-
 </style>
