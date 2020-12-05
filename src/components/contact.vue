cc98d52506d0717a33d4f3346d7ab7d06ae609cd
nakamura0736 <tel07360102@gmail.com>
2020-12-05 14:02:28 +0900

    modify layout of contact page

--- src/components/contact.vue
+++ src/components/contact.vue
@@ -4,37 +4,28 @@
          <h1>ご相談はこちら</h1>
       </div>
       <div id='pg-body'>
-         <div class='q-category pb-4'>
-            <h3 class='text-primary mb-4'>相談フォーム</h3>
+         <div class='pb-4'>
+            <h5>相談をご希望の方は以下のフォームへ入力いただくか、メール、お電話にてご連絡ください。</h5>
+         </div>
+         <div class='items pb-4'>
             <div class='items pb-4'>
-               <!-- <h5 class='border-bottom'>相談フォームへご記入ください</h5> -->
-               <h5>相談フォームへご記入ください</h5>
                <a href='https://forms.gle/2EsXZ91s8SivLL4p6' class='btn btn-primary' type='dark' target='_blank' rel='noopener noreferrer'>ご相談フォーム</a>
             </div>
          </div>
-         <div class='q-category pb-4'>
-            <h3 class='text-primary mb-4'>電話・FAX・メール</h3>
-            <div class='items pb-4'>
-               <h5 class='border-bottom'>電話番号</h5>
-               <div>
-                  <p>
-                     <a href="tel:117">080-0000-0000</a>
-                  </p>
-               </div>
-            </div>
-            <div class='items pb-4'>
-               <h5 class='border-bottom'>FAX</h5>
+         <div class='items pb-4'>
+            <h3 class='border-bottom'>電話番号</h3>
+            <div>
                <p>
                   <a href="tel:117">080-0000-0000</a>
                </p>
             </div>
-            <div class='items pb-4'>
-               <h5 class='border-bottom'>メールアドレス</h5>
-               <p>
-                  <a href='mailto:xxxxxxx@gmail.com'>xxxxxxx&#64;gmail.com</a>
-               </p>
-            </div>
-         </div>       
+         </div>
+         <div class='items pb-4'>
+            <h3 class='border-bottom'>メールアドレス</h3>
+            <p>
+               <a href='mailto:xxxxxxx@gmail.com'>xxxxxxx&#64;gmail.com</a>
+            </p>
+         </div>
       </div>
    </div>
 
