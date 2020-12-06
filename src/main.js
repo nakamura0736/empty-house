51798f1b1507da3b83e2665a75d6e5a98bf24767
nakamura0736 <tel07360102@gmail.com>
2020-12-06 16:11:43 +0900

    make it available at IE11

--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+require("@babel/polyfill");
+import "@babel/polyfill";
+
 import App from './App'
 import router from './router'
 
