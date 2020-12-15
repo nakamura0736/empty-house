00259144e3eaf4b96c82ca13b02a8c355357aace
nakamura0736 <tel07360102@gmail.com>
2020-12-16 00:25:36 +0900

    pictures on each cards and adjust responsive

--- src/components/concept.vue
+++ src/components/concept.vue
@@ -9,38 +9,35 @@
          </div>
          <div class="card-columns">
             <div class="card">
-               <svg class="bd-placeholder-img card-img-top" width="100%" height="160" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: Image cap"><title>Placeholder</title><rect fill="#868e96" width="100%" height="100%"/><text fill="#dee2e6" dy=".3em" x="50%" y="50%">Image cap</text></svg>
+               <img class="card-img-top" src="../assets/img/empty-house.png" alt="Card image cap">
                <div class="card-body">
                   <h5 class="card-title">空き家を何とかしたい</h5>
                </div>
             </div>
             <div class="card">
-               <svg class="bd-placeholder-img card-img-top" width="100%" height="160" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: Image cap"><title>Placeholder</title><rect fill="#868e96" width="100%" height="100%"/><text fill="#dee2e6" dy=".3em" x="50%" y="50%">Image cap</text></svg>
+               <img class="card-img-top" src="../assets/img/tearing-down-house.png" alt="Card image cap">
                <div class="card-body">
                   <h5 class="card-title">空き家を取り壊すのに何百万もかかるので悩んでいる</h5>
                </div>
             </div>
             <div class="card">
-               <svg class="bd-placeholder-img card-img-top" width="100%" height="160" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: Image cap"><title>Placeholder</title><rect fill="#868e96" width="100%" height="100%"/><text fill="#dee2e6" dy=".3em" x="50%" y="50%">Image cap</text></svg>
+               <img class="card-img-top" src="../assets/img/annoying-house.png" alt="Card image cap">
                <div class="card-body">
                   <h5 class="card-title">私の空き家がご近所に迷惑をかけている</h5>
                </div>
             </div>
             <div class="card">
-               <svg class="bd-placeholder-img card-img-top" width="100%" height="160" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: Image cap"><title>Placeholder</title><rect fill="#868e96" width="100%" height="100%"/><text fill="#dee2e6" dy=".3em" x="50%" y="50%">Image cap</text></svg>
+               <img class="card-img-top" src="../assets/img/inherited-house.png" alt="Card image cap">
                <div class="card-body">
                   <h5 class="card-title">親から相続した家が空き家になって困っている</h5>
                </div>
             </div>
             <div class="card">
-               <svg class="bd-placeholder-img card-img-top" width="100%" height="160" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: Image cap"><title>Placeholder</title><rect fill="#868e96" width="100%" height="100%"/><text fill="#dee2e6" dy=".3em" x="50%" y="50%">Image cap</text></svg>
+               <img class="card-img-top" src="../assets/img/forniture.png" alt="Card image cap">
                <div class="card-body">
                   <h5 class="card-title">空き家の中にある不用品を処分したい</h5>
                </div>
             </div>
-
-
-
          </div>
       </div>
    </div>
@@ -51,31 +48,34 @@
 </script>
 
 <style>
-/* .card-columns { */
-   @media screen and ( max-width:543px) { /* xs */
-      .card-columns {
-         column-count: 1;
-      }
+
+@media screen and ( max-width:767px) { /* sm */
+   .card-columns {
+      column-count: 1;
    }
-   @media screen and (min-width:544px) and ( max-width:767px) { /* sm */
-      .card-columns {
-         column-count: 2;
-      }
+   .card {
+      /* height: 400px; */
    }
-   @media screen and (min-width:768px) and ( max-width:991px) { /* md */
-      .card-columns {
-         column-count: 2;
-      }
+   .card-img-top {
+      /* height: 300px; */
    }
-   @media screen and (min-width:992px) and ( max-width:1199px) { /* lg */
-      .card-columns {
-         /* column-count: 4; */
-      }
+}
+@media screen and (min-width:768px) and ( max-width:991px) { /* md */
+   .card {
+      height: 280px;
    }
-   @media screen and (min-width:1200) { /* xl */
-      .card-columns {
-         /* column-count: 5; */
-      }
+   .card-columns {
+      column-count: 2;
    }
-/* } */
+}
+@media screen and (min-width:992px) and (max-width:1199px)  { /* lg */
+   .card {
+      height: 260px;
+   }
+}
+@media screen and (min-width:1200px)  { /* lg */
+   .card {
+      height: 300px;
+   }
+}
 </style>
