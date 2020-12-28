c144741ae1725249ba368bb5255c722025edea93
nakamura0736 <tel07360102@gmail.com>
2020-12-28 14:54:31 +0900

    background image modify

--- src/components/home.vue
+++ src/components/home.vue
@@ -1,10 +1,7 @@
 <template>
    <div>
-      <div class="fixed-wrap">
-         <div class="fixed">
+      <div class='jumbotron'>
          <h1 class='text-light'>あなたの「実家」は「空き家」になっていませんか？</h1>
-            
-         </div>
       </div>
    </div>
 </template>
@@ -13,13 +10,9 @@
 </script>
 
 <style>
-.fixed {
-   height: 600px;
-   width: 100%;
-   z-index: -1;
-   background-image: url(../assets/img/home.jpg);
-   background-attachment: fixed;
+.jumbotron {
+   height: 800px;
+   background:url(../assets/img/home.png);
    background-size: cover;
-   /* background-size: 450px 180px; */
 }
 </style>
